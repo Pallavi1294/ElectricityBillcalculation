f5458996309125ee5152971710c233dd50edd729
MacStudent <macstudent@20x-imacxx-m-397.local>
2018-08-09 16:31:48 -0400

    day 2

--- ElectricityBillcalculation/BillDetailViewController.swift
+++ ElectricityBillcalculation/BillDetailViewController.swift
@@ -9,16 +9,31 @@
 import UIKit
 
 class BillDetailViewController: UIViewController {
-
+var electricityBill: ElectricityBill?
     override func viewDidLoad() {
         super.viewDidLoad()
-
+self.navigationItem.title = "Bill Detail"
         // Do any additional setup after loading the view.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    private func calculateTotalBillAmount(){
+        var totalBill =  0.0
+        if (electricityBill?.unitConsumed)! <= 100 {
+            
+            totalBill = Double((electricityBill?.unitConsumed)!) * 0.75
+        }
+        else if ((electricityBill?.unitConsumed)!) <= 250{
+            totalBill = 75 + (Double((electricityBill?.unitConsumed)!) - 100) * 1.25
+            
+        }
+        else (){
+            
+        }
     }
     
 
--- ElectricityBillcalculation/ElectricityBillViewController.swift
+++ ElectricityBillcalculation/ElectricityBillViewController.swift
@@ -11,8 +11,18 @@ import UIKit
 class ElectricityBillViewController: UIViewController {
 
     
+    var electricityBill: ElectricityBill?
     
-    
+    @IBOutlet weak var txtTotal: UITextField!
+    @IBOutlet weak var txtBillDate: UITextField!
+    @IBOutlet weak var txtTotalUnit: UITextField!
+    @IBOutlet weak var seggend: UISegmentedControl!
+    @IBOutlet weak var txtGender: UITextField!
+    @IBOutlet weak var txtCustname: UITextField!
+    @IBOutlet weak var txtCustid: UITextField!
+    @IBAction func btnTotal(_sender: Any) {
+        electricityBill = ElectricityBill(customerId: Int(txtCustid.text!), customerName: txtCustname.text!, gender:Gender.MALE, billDate:Date(), unitConsumed: Int(txtTotalUnit.text!), totalBillAmount: 0)
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -29,14 +39,16 @@ class ElectricityBillViewController: UIViewController {
     
     
     
-    /*
+    
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     
         // Get the new view controller using segue.destinationViewController.
         // Pass the selected object to the new view controller.
     }
-    */
+    
 
 }
